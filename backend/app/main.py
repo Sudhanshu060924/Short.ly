686695e53f0b4a2825e7250b8eca56503a9f7d0d
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-07-28 22:01:46 +0530

    debug: allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,8 +16,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-         "http://localhost:5173",
-        "https://short-ly-rosy.vercel.app",
+        "http://localhost:5173",
+        os.getenv("FRONTEND_URL")
     ],
     allow_credentials=True,
     allow_methods=["*"],
