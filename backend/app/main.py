3ee42680b8b22cf79bb1019272da98969dce6f6d
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-07-28 21:26:23 +0530

    fix: add vercel domain to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
+import os
+
 from fastapi import FastAPI
 
 from app.db.database import Base, engine
 from app.models.url import URL
 from app.routes.url_routes import router as url_routes
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,7 +16,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        os.getenv("FRONTEND_URL")
     ],
     allow_credentials=True,
     allow_methods=["*"],
